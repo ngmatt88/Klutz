686690bf7bb62593a4b1001cd056b1d8429eb0db
matt.ng <ngmatt1988@gmail.com>
2015-07-05 13:22:50 -0400

    Fix to home buttons not working after switching fragments.

--- app/src/main/java/com/duckwarlocks/klutz/MainActivity.java
+++ app/src/main/java/com/duckwarlocks/klutz/MainActivity.java
@@ -30,13 +30,7 @@ import java.sql.SQLException;
 
 public class MainActivity extends ActionBarActivity {
 
-    private GpsCoordinatesHelper gps;
-    private String mCityName;
-    private double mLatitude;
-    private double mLongitude;
     private Toolbar toolbar;
-    private TextView mCurCoordinateTxtView;
-    private LocationsDAO mLocationDAO;
 
     String TITLES[] = {"Home","Saved Locations","Recent"};
     int ICONS[] = {R.drawable.home_icon,R.drawable.fish_bowl,R.drawable.ic_star24dp};
@@ -87,141 +81,10 @@ public class MainActivity extends ActionBarActivity {
                 // Code here will execute once drawer is closed
             }
 
-
-
         }; // Drawer Toggle Object Made
         Drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
         mDrawerToggle.syncState();               // Finally we set the drawer toggle sync State
 
-
-
-        mLocationDAO = new LocationsDAO(this);
-        mCurCoordinateTxtView = (TextView)findViewById(R.id.currentCoordinates);
-
-    }
-
-
-    /**
-     * Grabs the current GPS coordinates and saves them to external file.
-     * @param view
-     */
-    public void getCoordinates(View view){
-        gps = new GpsCoordinatesHelper(this);
-
-        if(gps.ismCanGetLocation()){
-            mLatitude = gps.getmLatitude();
-            mLongitude = gps.getmLongitude();
-            mCityName = gps.getmCityName();
-
-            displayCurrentCoordinates();
-        }else{
-            gps.showSettingsAlert();
-        }
-    }
-
-    /**
-     * Displays Coordinates in the sub-title textview
-     */
-    private void displayCurrentCoordinates(){
-        mCurCoordinateTxtView.setText(CommonConstants.LATITUDE_ABBREV + " : " + mLatitude + " " + CommonConstants.LONGITUDE_ABBREV + " : " + mLongitude);
-    }
-
-
-
-    /**
-     *
-     * @param locationVO
-     */
-    private void saveToFile(LocationVO locationVO){
-        if(locationVO.getmName().equals("")){
-            Toast.makeText(getApplicationContext(),"Invalid Name",Toast.LENGTH_LONG).show();
-        }else{
-            try{
-                FileHelper.writeNewRecordToFile(locationVO);
-                Toast.makeText(getApplicationContext(),"Coordinates Saved",Toast.LENGTH_LONG).show();
-            }catch (StopProcessingException e){
-                 e.printStackTrace();
-            }
-        }
-    }
-
-    private LocationVO createLocationVO(String nameTitle){
-        LocationVO locationVO = new LocationVO();
-        locationVO.setmName(nameTitle);
-        locationVO.setmLatitude(mLatitude);
-        locationVO.setmLongitude(mLongitude);
-        locationVO.setmCity(mCityName);
-
-        return locationVO;
-    }
-
-    /**
-     * Prompt for the name to be given to these coordinates
-     * @param view
-     * @return
-     */
-    public void promptCoordinateName(View view){
-        String defaultStr = mCurCoordinateTxtView.getText().toString();
-        String subTitleStr = getResources().getString(R.string.subTitle);
-        if(defaultStr.equals(subTitleStr)){
-            AlertDialogHelper.buildWarningAlert(this,
-                    "Current Location Not Found",
-                    "Please Get Current Location Then Try Again",false,"OK").show();
-        }else{
-            String alertTitle = getResources().getString(R.string.coordinateNameTitle);
-            String msg = getResources().getString(R.string.coordinateNameMsg);
-
-            AlertDialog.Builder theAlert = AlertDialogHelper.buildAlert(this,alertTitle,msg,false);
-
-            final EditText input = new EditText(MainActivity.this);
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT
-            );
-            input.setLayoutParams(layoutParams);
-            theAlert.setView(input);
-
-            theAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    String title = input.getText().toString();
-
-                    if (title != null && !title.equals("")) {
-
-//                        saveToFile(createLocationVO(title));
-                        saveToDB(createLocationVO(title));
-
-                        Toast.makeText(getApplicationContext(), "Your location is - Latitude : " + mLatitude +
-                                "and Longitude : " + mLongitude, Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-
-            theAlert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.cancel();
-                }
-            });
-            theAlert.show();
-        }
-    }
-
-    private void saveToDB(LocationVO location){
-        try{
-            mLocationDAO.open();
-
-            mLocationDAO.createLocationVO(
-                    location.getmName(), Double.toString(location.getmLatitude()),
-                    Double.toString(location.getmLongitude()), location.getmCity());
-            mLocationDAO.close();
-        }catch(SQLException e){
-            e.printStackTrace();
-            System.exit(1);
-        }
-        Toast saveToast = Toast.makeText(this, "Your Searches Have Been Saved", Toast.LENGTH_LONG);
-        saveToast.setGravity(Gravity.CENTER, 0, 0);
-        saveToast.show();
     }
 
     @Override
