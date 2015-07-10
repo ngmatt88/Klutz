c7784ce6c761cf91f9107c893b66c230771b345f
Rafael Garcia <rafael.garcia@quadev.mx>
2015-07-10 09:18:47 -0400

    Delete DrawerAdapter that was using a Recycler view

--- app/src/main/java/com/duckwarlocks/klutz/MainActivity.java
+++ app/src/main/java/com/duckwarlocks/klutz/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
