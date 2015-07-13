63471fa3ee0b9466e571db715cdac33b5fc11f13
matt.ng <ngmatt1988@gmail.com>
2015-07-13 17:26:31 -0400

    Added method in PrettyButtonView to resize imageview to shape/size of image.

--- app/src/main/java/com/duckwarlocks/klutz/views/PrettyButtonView.java
+++ app/src/main/java/com/duckwarlocks/klutz/views/PrettyButtonView.java
@@ -2,7 +2,12 @@ package com.duckwarlocks.klutz.views;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -40,10 +45,69 @@ public class PrettyButtonView extends RelativeLayout {
 
     public void setmBtnImage(int resourceId){
         mBtnImage.setImageResource(resourceId);
+
+        scaleImage();
     }
 
     public void setSpeechTxt(String speechTxt){
         mSpeechBubble.setBackgroundResource(R.drawable.speechbubble);
         mSpeechBubble.setText(speechTxt);
+    }
+    private void scaleImage()
+    {
+        // Get the ImageView and its bitmap
+        ImageView view = (ImageView) findViewById(R.id.prettyBtnImg);
+        Drawable drawing = view.getDrawable();
+        if (drawing == null) {
+            return; // Checking for null & return, as suggested in comments
+        }
+        Bitmap bitmap = ((BitmapDrawable)drawing).getBitmap();
+
+        // Get current dimensions AND the desired bounding box
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int bounding = dpToPx(250);
+        Log.i("Test", "original width = " + Integer.toString(width));
+        Log.i("Test", "original height = " + Integer.toString(height));
+        Log.i("Test", "bounding = " + Integer.toString(bounding));
+
+        // Determine how much to scale: the dimension requiring less scaling is
+        // closer to the its side. This way the image always stays inside your
+        // bounding box AND either x/y axis touches it.
+        float xScale = ((float) bounding) / width;
+        float yScale = ((float) bounding) / height;
+        float scale = (xScale <= yScale) ? xScale : yScale;
+        Log.i("Test", "xScale = " + Float.toString(xScale));
+        Log.i("Test", "yScale = " + Float.toString(yScale));
+        Log.i("Test", "scale = " + Float.toString(scale));
+
+        // Create a matrix for the scaling and add the scaling data
+        Matrix matrix = new Matrix();
+        matrix.postScale(scale, scale);
+
+        // Create a new bitmap and convert it to a format understood by the ImageView
+        Bitmap scaledBitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
+        width = scaledBitmap.getWidth(); // re-use
+        height = scaledBitmap.getHeight(); // re-use
+        BitmapDrawable result = new BitmapDrawable(scaledBitmap);
+        Log.i("Test", "scaled width = " + Integer.toString(width));
+        Log.i("Test", "scaled height = " + Integer.toString(height));
+
+        // Apply the scaled bitmap
+        view.setImageDrawable(result);
+
+        // Now change ImageView's dimensions to match the scaled image
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) view.getLayoutParams();
+        params.width = width;
+        params.height = height;
+        view.setLayoutParams(params);
+
+        Log.i("Test", "done");
+    }
+
+    private int dpToPx(int dp)
+    {
+        float density = getContext().getResources().getDisplayMetrics().density;
+        return Math.round((float)dp * density);
     }
 }
