445cc525e6084655673383a3410dc6f7d2ea0e0a
Rafael Garcia <rafael.garcia@quadev.mx>
2015-07-13 22:41:15 -0400

    pinapple without blank bg

---
+++ app/src/main/java/com/duckwarlocks/klutz/views/MyTextView.java
@@ -0,0 +1,29 @@
+package com.duckwarlocks.klutz.views;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by raf0c on 13/07/15.
+ */
+public class MyTextView extends TextView {
+
+    public MyTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public void setTypeface(Typeface tf) {
+
+            super.setTypeface(Typeface.createFromAsset(getContext().getAssets(), "fonts/ARNORG__.TTF"));
+
+    }
+}
